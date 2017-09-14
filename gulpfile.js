6d0b3ed416e929d536721d54a7c7bfe38cc45344
Oleg Timofeev <oleg.timofeev20@gmail.com>
2017-09-14 17:44:01 +0300

    added gult auto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function () {
-    console.log('hello Oleg');
+gulp.task('build-all', function () {
+    gulp.src('lib/mrm.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('dist/css'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch('lib/**/*.scss', ['build-all']);
+});
+
+gulp.task('default', ['build-all']);
