f0c76092a9aee6c4459ae5c95e7a64d1f87b798e
Oleg Timofeev <oleg.timofeev20@gmail.com>
2017-09-14 23:26:56 +0300

    14.09

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let cleanCSS = require('gulp-clean-css');
 let sourcemaps = require('gulp-sourcemaps');
+let minify = require('gulp-minify');
 
 gulp.task('build-all', () => {
     gulp.src('lib/mrm.scss')
@@ -9,14 +10,36 @@ gulp.task('build-all', () => {
         .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('minimize-all', () =>{
+gulp.task('minimize-css', () => {
     gulp.src(['dist/css/*.css', '!dist/css/*.min.css'])
-    .pipe(sourcemaps.init())
-    .pipe(cleanCSS({debug: true, format: 'beautify'}))
-    .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest('dist/css'));
-})
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS({
+            debug: true,
+            format: 'beautify'
+        }, function(data){
+            let oldSize = data.stats.originalSize,
+                newSize = data.stats.minifiedSize,
+                name = data.name,
+                delta = Math.round(100 - newSize / oldSize * 100);
+
+                console.log(`${name}: ${oldSize} -> ${newSize} (${delta}%)`);
+        }))
+        .pipe(sourcemaps.write(''))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minimaze-js', () => {
+    gulp.src(['dist/js/*.js', '!dist/js/*.min.js'])
+        .pipe(minify({
+            ext:{
+                min:'.min.js'
+            },
+            exclude: ['tasks'],
+            ignoreFiles: ['-min.js']
+        }))
+        .pipe(gulp.dest('dist/js'));
+});
 
 gulp.task('watch', () => gulp.watch('lib/**/*.scss', ['build-all']));
 
-gulp.task('default', ['build-all', 'minimize-all']);
+gulp.task('default', ['build-all', 'minimize-css', 'minimaze-js']);
