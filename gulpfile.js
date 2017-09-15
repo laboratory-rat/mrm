c22e2732f2dc02566a81d742ea32537f7548e1f0
Oleg Timofeev <oleg.timofeev20@gmail.com>
2017-09-15 17:27:56 +0300

    15.09

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,13 @@ gulp.task('minimize-css', () => {
         .pipe(cleanCSS({
             debug: true,
             format: 'beautify'
-        }, function(data){
+        }, function (data) {
             let oldSize = data.stats.originalSize,
                 newSize = data.stats.minifiedSize,
                 name = data.name,
                 delta = Math.round(100 - newSize / oldSize * 100);
 
-                console.log(`${name}: ${oldSize} -> ${newSize} (${delta}%)`);
+            console.log(`${name}: ${oldSize} -> ${newSize} (${delta}%)`);
         }))
         .pipe(sourcemaps.write(''))
         .pipe(gulp.dest('dist/css'));
@@ -31,8 +31,8 @@ gulp.task('minimize-css', () => {
 gulp.task('minimaze-js', () => {
     gulp.src(['dist/js/*.js', '!dist/js/*.min.js'])
         .pipe(minify({
-            ext:{
-                min:'.min.js'
+            ext: {
+                min: '.min.js'
             },
             exclude: ['tasks'],
             ignoreFiles: ['-min.js']
@@ -40,6 +40,15 @@ gulp.task('minimaze-js', () => {
         .pipe(gulp.dest('dist/js'));
 });
 
+gulp.task('copy', function () {
+    gulp.src('./dist/js/*')
+        .pipe(gulp.dest('web/assets/js'));
+    gulp.src('./dist/css/*')
+        .pipe(gulp.dest('web/assets/css'));
+    gulp.src('./dist/css/fonts/*')
+        .pipe(gulp.dest('web/assets/css/fonts'))
+});
+
 gulp.task('watch', () => gulp.watch('lib/**/*.scss', ['build-all']));
 
-gulp.task('default', ['build-all', 'minimize-css', 'minimaze-js']);
+gulp.task('default', ['build-all', 'minimaze-js', 'copy']);
